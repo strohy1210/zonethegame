d94e218ea270fa58af6215ca0a2d9e3e1a4d146c
Sarah Stroh <strohsm88@gmail.com>
2016-05-03 18:22:33 -0400

    some more data

--- app/analytics.js
+++ app/analytics.js
@@ -1,1 +1,26 @@
-analytics.js
+var ref = new Firebase('https://flickering-fire-1102.firebaseio.com/');
+// var activeUsers = new Firebase('https://airpair-analytics-tutorial.firebaseio.com/activeUsers');
+var activeVisitors = ref.child('activeVisitors');
+activeVisitors.push({
+  path: window.location.pathname,
+  arrivedAt: Firebase.ServerValue.TIMESTAMP,
+  userAgent: navigator.userAgent
+});
+
+var usersRef = ref.child("users");
+
+// usersRef.set({
+//   alanisawesome: {
+//     date_of_birth: "June 23, 1912",
+//     full_name: "Alan Turing"
+//   },
+//   gracehop: {
+//     date_of_birth: "December 9, 1906",
+//     full_name: "Grace Hopper"
+//   }
+// });
+
+usersRef.update({
+  "alanisawesome/full_name": "Alan The Machine"
+  "gracehop/full_name": "Amazing Grace"
+});
