5f95e9ca1b8744a2490b7a23064ee9e5b551dd6a
strohy1210 <sarah.stroh@flatironschool.com>
2016-05-20 11:32:54 -0400

    i think this is as good as it gets for now

--- app/assets/analytics.js
+++ app/assets/analytics.js
@@ -1,17 +1,27 @@
+
 var ref = new Firebase('https://flickering-fire-1102.firebaseio.com/');
 
 
 var usersRef = ref.child("users");
 
 
-usersRef.orderByChild("score").on("value", function(snap) {
+function formatTime(seconds) {
+  var date = new Date(null);
+  date.setSeconds(seconds); // specify value for SECONDS here
+  return date.toISOString().substr(11, 8).slice(4,8);
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+usersRef.orderByChild("score").on("value", function(snap) { 
 
   var users = _.values(snap.val());
   var usersInOrder = _.sortBy( users, 'score' ).reverse();
   var j=0;
-  for (j; j <= users.length; j++) {       
-    $("tbody").append("<tr><td>" + parseInt(j+1) + ".</td><td>" + usersInOrder[j].first_name + "</td><td>" + usersInOrder[j].score + "</td></tr>");
+  for (j; j <= users.length; j++) {  
+    $("tbody").append("<tr><td align='center'>" + parseInt(j+1) + "</td><td align='center' class='player-icon'> <img src='" + usersInOrder[j].image_url + "'> </td><td>" + usersInOrder[j].first_name + "</td><td>" + numberWithCommas(usersInOrder[j].score) +  "</td><td>" + formatTime(usersInOrder[j].time) + "</td></tr>");
   }
-
 
 });
